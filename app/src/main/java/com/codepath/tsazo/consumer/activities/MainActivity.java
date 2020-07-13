3047dbfc914e99273e02bc83791132bbecb102ed
tsazo <trinitysazo@gmail.com>
2020-07-13 09:56:44 -0700

    Fixed a bug and now all users can be either a driver or a user depending on their choice.

--- app/src/main/java/com/codepath/tsazo/consumer/activities/MainActivity.java
+++ app/src/main/java/com/codepath/tsazo/consumer/activities/MainActivity.java
@@ -3,14 +3,22 @@ package com.codepath.tsazo.consumer.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.codepath.tsazo.consumer.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
