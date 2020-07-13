6c24f3787eca784a5aad779244a63905b3b7f06e
tsazo <trinitysazo@gmail.com>
2020-07-13 08:34:25 -0700

    Added a flow to two separate activities based on the User's driver status.

---
+++ app/src/main/java/com/codepath/tsazo/consumer/activities/DriverMainActivity.java
@@ -0,0 +1,16 @@
+package com.codepath.tsazo.consumer.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.codepath.tsazo.consumer.R;
+
+public class DriverMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_main);
+    }
+}
