6299158caed9bc71b821fa3d3476eeb345f8cc98
tsazo <trinitysazo@gmail.com>
2020-07-15 10:55:49 -0700

    Added map fragment to order details view for the driver.

--- app/src/main/java/com/codepath/tsazo/consumer/activities/DriverOrderDetailsActivity.java
+++ app/src/main/java/com/codepath/tsazo/consumer/activities/DriverOrderDetailsActivity.java
@@ -1,16 +1,148 @@
 package com.codepath.tsazo.consumer.activities;
 
+import android.Manifest;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.codepath.tsazo.consumer.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import okhttp3.Headers;
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.RuntimePermissions;
+
+import static com.google.android.gms.location.LocationServices.getFusedLocationProviderClient;
+
+@RuntimePermissions
 public class DriverOrderDetailsActivity extends AppCompatActivity {
+
+    private static final String TAG = "DriverOrderDetails";
+    private SupportMapFragment mapFragment;
+    private String FIND_PLACE_URL = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=";
+    private GoogleMap driverMap;
+    private Location mCurrentLocation;
+    public static JSONObject location;
+    private String address;
+
+    private final static String KEY_LOCATION = "location";
+
+
+    /*
+     * Define a request code to send to Google Play services This code is
+     * returned in Activity.onActivityResult
+     */
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_order_details);
+
+        if (savedInstanceState != null && savedInstanceState.keySet().contains(KEY_LOCATION)) {
+            // Since KEY_LOCATION was found in the Bundle, we can be sure that mCurrentLocation
+            // is not null.
+            mCurrentLocation = savedInstanceState.getParcelable(KEY_LOCATION);
+        }
+
+        mapFragment = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.driverMap));
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(new OnMapReadyCallback() {
+                @Override
+                public void onMapReady(GoogleMap map) {
+                    loadMap(map);
+                }
+            });
+        } else {
+            Toast.makeText(this, "Error - Map Fragment was null!!", Toast.LENGTH_SHORT).show();
+        }
     }
+
+    protected void loadMap(GoogleMap googleMap) {
+        driverMap = googleMap;
+        if (driverMap != null) {
+            // Map is ready
+            Toast.makeText(this, "Map Fragment was loaded properly!", Toast.LENGTH_SHORT).show();
+            DriverOrderDetailsActivityPermissionsDispatcher.getMyLocationWithPermissionCheck(this);
+            //DriverOrderDetailsActivityPermissionsDispatcher.startLocationUpdatesWithPermissionCheck(this);
+        } else {
+            Toast.makeText(this, "Error - Map was null!!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        DriverOrderDetailsActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+    }
+
+    @SuppressWarnings({"MissingPermission"})
+    @NeedsPermission({Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION})
+    public void getMyLocation() {
+        // Access users current location
+        FusedLocationProviderClient locationClient = getFusedLocationProviderClient(this);
+        locationClient.getLastLocation()
+                .addOnSuccessListener(new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            Log.i(TAG, "Location: " + location.toString());
+                            //getLocationFromCoords(location.getLatitude(), location.getLongitude());
+                        }
+                    }
+                })
+
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "Error trying to get last GPS location");
+                        e.printStackTrace();
+                    }
+                });
+    }
+
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putParcelable(KEY_LOCATION, mCurrentLocation);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+
+//    private void getLocationFromCoords(double x, double y) {
+//        AsyncHttpClient client = new AsyncHttpClient();
+//        String geoUrl = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + x + "," + y + "&key=" + getString(R.string.google_maps_api_key);
+//        Log.i(TAG, geoUrl);
+//        client.get(geoUrl, new JsonHttpResponseHandler() {
+//            @Override
+//            public void onSuccess(int statusCode, Headers headers, JSON json) {
+//                Log.i(TAG, getString(R.string.google_maps_api_key));
+//                Log.i(TAG, "Response" + " " + json.toString());
+//                location = json.jsonObject;
+//                try {
+//                    address = ((JSONObject) location.getJSONArray("results").get(0)).getString("formatted_address");
+//                    Log.i(TAG, address);
+//                    //TODO: Assign address to store or user or driver
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+//
+//            }
+//
+//            @Override
+//            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+//
+//            }
+//        });
+//    }
 }
